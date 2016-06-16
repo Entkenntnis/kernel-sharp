931fc3b7771977abd29fad2235537d2a35d4e14d
David Li <david.li@tum.de>
2016-06-16 12:44:28 +0200

    moving all parameters out of parser into provider classes

--- src/core/Parser.cs
+++ src/core/Parser.cs
@@ -7,80 +7,6 @@ using System.Text.RegularExpressions;
 namespace Kernel
 {
 
-    public class ParseException : Exception
-    {
-        public ParseException(string message) : base(message){ }
-    }
-
-    public class TokenDefinition
-    {
-        public Regex Rule { get; private set; }
-        public string Label { get; private set; }
-        public bool Ignore { get; private set; }
-        public int Precedence { get; private set; }
-        public TokenDefinition(Regex rule, string label, int prec = 0, bool ignore = false)
-        {
-            Rule = rule;
-            Label = label;
-            Ignore = ignore;
-            Precedence = prec;
-        }
-    }
-
-    public class Token
-    {
-        public string Value { get; private set; }
-        public string Label { get; private set; }
-        public Token(string value, string label)
-        {
-            Value = value;
-            Label = label;
-        }
-        public override string ToString()
-        {
-            return string.Format("[Token: Value={0}, Label={1}]", Value, Label);
-        }
-    }
-
-    public class TokenProvider
-    {
-        public static List<TokenDefinition> getDefinitions()
-        {
-            List<TokenDefinition> definitions = new List<TokenDefinition>();
-            definitions.Add(new TokenDefinition(new Regex(@"[\s]+"), "whitespace", 100, true));
-            definitions.Add(new TokenDefinition(new Regex(@"\;[^\n]*(?:\n|$)"), "comment", 90, true));
-            definitions.Add(new TokenDefinition(new Regex(@"""[^""\\]*(?:\\.[^""\\]*)*"""), "string", 80));
-            definitions.Add(new TokenDefinition(new Regex(@"\(\)"), "nil", 70));
-            definitions.Add(new TokenDefinition(new Regex(@"\("), "openpa", 60));
-            definitions.Add(new TokenDefinition(new Regex(@"\)"), "clospa", 50));
-            definitions.Add(new TokenDefinition(new Regex(@"\.(?=[\s\(])"), "dot", 40));
-            definitions.Add(new TokenDefinition(new Regex(@"[^\s\(\)\;]+"), "text", 30));
-            return definitions;
-        }
-    }
-
-    public class TokenStream
-    {
-        private int index;
-        private List<Token> data;
-        public TokenStream(List<Token> input)
-        {
-            data = new List<Token>(input);
-            index = 0;
-        }
-        public Token Next()
-        {
-            if (index < data.Count)
-            {
-                return data[index++];
-            }
-            else
-            {
-                return null;
-            }
-        }
-    }
-
     public class Parser
     {
         public static List<Token> Lex(string datum)
@@ -88,33 +14,30 @@ namespace Kernel
             int curIndex = 0;
             List<Token> tokens = new List<Token>();
             List<TokenDefinition> definitions = TokenProvider.getDefinitions();
-            definitions.Sort((a,b)=>{return b.Precedence - a.Precedence;});
-            while (curIndex < datum.Length)
-            {
+            definitions.Sort((a, b) => {
+                return b.Precedence - a.Precedence;
+            });
+            while (curIndex < datum.Length) {
                 bool success = false;
-                foreach (var rule in definitions)
-                {
+                foreach (var rule in definitions) {
                     Match match = rule.Rule.Match(datum, curIndex);
-                    if (match.Success && match.Index == curIndex)
-                    {
+                    if (match.Success && match.Index == curIndex) {
                         success = true;
-                        if (!rule.Ignore)
-                        {
+                        if (!rule.Ignore) {
                             tokens.Add(new Token(match.Value, rule.Label));
                         }
                         curIndex += match.Length;
                         break;
                     }
                 }
-                if (!success)
-                {
+                if (!success) {
                     throw new ParseException("Unrecognised pattern");
                 }
             }
             return tokens;
         }
 
-        public static object Read(TokenStream s)
+        private static object read(TokenStream s)
         {
             Token cur = s.Next();
             if (null == cur)
@@ -123,73 +46,33 @@ namespace Kernel
                 return ')';
             if ("dot" == cur.Label)
                 return '.';
-            if ("openpa" != cur.Label)
-            {
-                // some more cases here
-                if ("nil" == cur.Label)
-                {
-                    return new KNil();
-                }
-                if ("string" == cur.Label)
-                {
-                    string sub = cur.Value.Substring(1, cur.Value.Length - 2);
-                    sub = sub.Replace("\\n", "\n").Replace("\\t", "\t").Replace("\\\"", "\"").Replace("\\r", "\r");
-                    return new KString(sub);
-                }
-                else if ("text" == cur.Label)
-                {
-                    if (cur.Value == "#t")
-                    {
-                        return new KBoolean(true);
+            if ("openpa" != cur.Label) {
+                var handler = TokenHandlerProvider.getHandler();
+                if (handler.ContainsKey(cur.Label)) {
+                    return handler[cur.Label](cur);
+                } else if ("text" == cur.Label) {
+                    var th = TextHandlerProvider.getHandler();
+                    th.Sort((a, b) => {
+                        return b.Precedance - a.Precedance;
+                    });
+                    foreach (TextHandler f in th) {
+                        object ret = f.Handler(cur);
+                        if (ret as KObject != null)
+                            return ret;
                     }
-                    if (cur.Value == "#f")
-                    {
-                        return new KBoolean(false);
-                    }
-                    if (cur.Value == "#empty-env")
-                    {
-                        return new KEnvironment();
-                    }
-                    if (cur.Value == "#ignore")
-                    {
-                        return new KIgnore();
-                    }
-                    if (cur.Value == "#inert")
-                    {
-                        return new KInert();
-                    }
-                    if (cur.Value == "#e-infinity")
-                        return new KDouble(Double.NegativeInfinity);
-                    if (cur.Value == "#e+infinity")
-                        return new KDouble(Double.PositiveInfinity);
-                    long x;
-                    if (long.TryParse(cur.Value, out x))
-                        return new KInteger(x);
-
-                    double d;
-                    if (!cur.Value.Contains("Infinity") &&  double.TryParse(cur.Value,NumberStyles.Float, CultureInfo.InvariantCulture, out d))
-                        return new KDouble(d);
-
-                    return new KSymbol(cur.Value);
-                }
-                else
-                {
+                    throw new ParseException("unhandled text");
+                } else {
                     throw new ParseException("unknown token");
                 }
-            }
-            else
-            {
+            } else {
                 // list stuff
                 List<object> content = new List<object>();
-                object v = Read(s);
+                object v = read(s);
                 bool dotted = false;
-                while (true)
-                {
-                    if ((v is char) && ((char)v) == '.')
-                    {
-                        v = Read(s);
-                        if (((char)Read(s)) != ')')
-                        {
+                while (true) {
+                    if ((v is char) && ((char)v) == '.') {
+                        v = read(s);
+                        if (((char)read(s)) != ')') {
                             throw new ParseException("Illegal use of dot");
                         }
                         if (v is KObject)
@@ -198,32 +81,24 @@ namespace Kernel
                             throw new ParseException("Illegal use of dot");
                         dotted = true;
                         break;
-                    }
-                    else if ((v is char) && ((char)v) == ')')
-                    {
+                    } else if ((v is char) && ((char)v) == ')') {
                         break;
-                    }
-                    else if (v is KObject)
-                    {
+                    } else if (v is KObject) {
                         content.Add(v);
-                        v = Read(s);
-                    }
-                    else
-                    {
+                        v = read(s);
+                    } else {
                         throw new ParseException("Parse error");
                     }
                 }
                 int index = content.Count - 1;
                 KPair tail;
-                if (dotted)
-                {
-                    tail = new KPair(content[index-1] as KObject,content[index] as KObject,false);
+                if (dotted) {
+                    tail = new KPair(content[index - 1] as KObject, content[index] as KObject, false);
                     index -= 2;
                 } else {
                     tail = new KPair(content[index--] as KObject, new KNil(), false);
                 }
-                for (int i = index; i >= 0; i--)
-                {
+                for (int i = index; i >= 0; i--) {
                     tail = new KPair(content[i] as KObject, tail, false);
                 }
                 return tail;
@@ -232,7 +107,7 @@ namespace Kernel
 
         public static KObject Parse(string tokens)
         {
-            KObject ret = Read(new TokenStream(Lex(tokens))) as KObject;
+            KObject ret = read(new TokenStream(Lex(tokens))) as KObject;
             if (null == ret)
                 throw new ParseException("Empty token!");
             return ret;
@@ -242,11 +117,10 @@ namespace Kernel
         {
             var output = new List<KObject>();
             TokenStream s = new TokenStream(Lex(tokens));
-            KObject cur = Read(s) as KObject;
-            while (cur != null)
-            {
+            KObject cur = read(s) as KObject;
+            while (cur != null) {
                 output.Add(cur);
-                cur = Read(s) as KObject;
+                cur = read(s) as KObject;
             }
             return output;
         }
